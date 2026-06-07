249a307ac56710b4c0b5fa278f4691afe0436913
Park-jelly <pjy041930@gmail.com>
2026-06-07 17:37:40 +0900

    환경변수 수정

--- fitness/src/main/java/com/example/fitness/FitnessApplication.java
+++ fitness/src/main/java/com/example/fitness/FitnessApplication.java
@@ -2,14 +2,8 @@ package com.example.fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// 아래 import 추가
-import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
-import org.springframework.ai.model.google.genai.autoconfigure.embedding.GoogleGenAiEmbeddingConnectionAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		GoogleGenAiChatAutoConfiguration.class,
-		GoogleGenAiEmbeddingConnectionAutoConfiguration.class
-})
+@SpringBootApplication
 public class FitnessApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessApplication.class, args);
